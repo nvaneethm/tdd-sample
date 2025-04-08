dc9c29237a1d335dae2a5f62920c8b37d0f876e1
Navaneeth M <navaneethna.m@gmail.com>
2025-04-08 14:24:28 +0530

    ⚙️ new line delimiter implementation

--- src/services/StringCalculator.js
+++ src/services/StringCalculator.js
@@ -3,10 +3,8 @@ export const addString = (numbers) => {
   if (numbers === "") {
     return 0
   }
-  if (!numbers.includes(",")) {
-    return parseInt(numbers, 10)
-  }
+  const delimiterRegex = /,|\n/
 
-  const nums = numbers.split(",")
+  const nums = numbers.split(delimiterRegex)
   return nums.reduce((sum, num) => sum + parseInt(num, 10), 0)
 }
