6afeeec1108744c58977af39724b693e76cc8d2b
Navaneeth M <navaneethna.m@gmail.com>
2025-04-08 14:15:56 +0530

    🟢 test case for single digit

--- src/tests/StringCalculator.test.js
+++ src/tests/StringCalculator.test.js
@@ -1,7 +1,11 @@
-import { addString } from "../services/StringCalculator";
+import { addString } from "../services/StringCalculator"
 
 describe("String Calculator", () => {
-    test("returns 0 for an empty string", () => {
-      expect(addString("")).toBe(0);
-    });
-  });
+  test("returns 0 for an empty string", () => {
+    expect(addString("")).toBe(0)
+  })
+  test("returns the number for a single number input", () => {
+    expect(addString("5")).toBe(5)
+    expect(addString("42")).toBe(42)
+  })
+})
