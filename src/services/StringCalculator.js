18b25cde996fc2eae8a285e1028f7c7b326e7f9b
Navaneeth M <navaneethna.m@gmail.com>
2025-04-08 14:28:51 +0530

    ⚙️ custom delimiter implementation

--- src/services/StringCalculator.js
+++ src/services/StringCalculator.js
@@ -3,7 +3,20 @@ export const addString = (numbers) => {
   if (numbers === "") {
     return 0
   }
-  const delimiterRegex = /,|\n/
+  let delimiterRegex = /,|\n/
+
+  if (numbers.startsWith("//")) {
+    const newlineIndex = numbers.indexOf("\n")
+    const customDelimiter = numbers.substring(2, newlineIndex)
+
+    const escapedDelimiter = customDelimiter.replace(
+      /[-\/\\^$*+?.()|[\]{}]/g,
+      "\\$&"
+    )
+    delimiterRegex = new RegExp(escapedDelimiter + "|\n")
+
+    numbers = numbers.substring(newlineIndex + 1)
+  }
 
   const nums = numbers.split(delimiterRegex)
   return nums.reduce((sum, num) => sum + parseInt(num, 10), 0)
