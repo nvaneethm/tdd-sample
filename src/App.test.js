7c19ceaa5efa1d25d3ff6565ddd31a99831417e7
nvaneethm <navaneethna.m@gmail.com>
2025-04-08 10:31:48 +0000

    Deploying to gh-pages from @ nvaneethm/tdd-sample@e0fa6935859b5a407409644ec349612d0c9e4865 🚀

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders String Calculator', () => {
-  render(<App />);
-  const stringElement = screen.getByText(/String Calculator/i);
-  expect(stringElement).toBeInTheDocument();
-});
